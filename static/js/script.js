49de79fcfa4838ce2d2118465804beb33d4d47e8
marcodldev <marcodelisi2@gmail.com>
2025-03-14 14:31:19 +0100

    + script

---
+++ static/js/script.js
@@ -0,0 +1,39 @@
+let guardia = document.getElementById("guardia");
+
+let ladro = document.getElementById("ladro");
+
+let stanza = document.getElementById("stanza");
+
+let esito = document.getElementById("esito");
+
+// BOTTONI
+
+let btnNord = document.getElementById("nord");
+let btnOvest = document.getElementById("ovest");
+let btnEst = document.getElementById("est");
+let btnSud = document.getElementById("sud");
+
+// FUNZIONI
+
+btnNord.addEventListener("click", nord);
+btnOvest.addEventListener("click", ovest);
+btnEst.addEventListener("click", est);
+btnSud.addEventListener("click", sud);
+
+
+function nord(){
+//    btnNord.style.top = btnNord.style.top + 50 ;
+}
+
+function ovest(){
+   
+}
+
+function est(){
+   
+}
+
+function sud(){
+   
+}
+
